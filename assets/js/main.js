d8e20f180dfb99ea3dd32a271a93528523d5bab5
Mohamed El-Bahrawy <51103024+MElbahrawy@users.noreply.github.com>
2024-08-05 20:36:59 +0300

    start

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+const accessKey = "iPB3dsnwupOX-qFq8EK5vhq8voY9TypXeyZbz7AcfuQ";
+const searchValue = document.querySelector("#search");
+const container = document.querySelector(".gallery");
+const showMore = document.querySelector(".show-more");
+let page = 1;
+
+const getData = async (keyword) => {
+  let api = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const res = await fetch(api).then((res) => res.json());
+  return res.results;
+};
+const createElement = (data) => {
+  data.map((pic) => {
+    const img = document.createElement("img");
+    img.className = "col-lg-3 col-md-5 col-sm-12 rounded-1";
+    img.src = pic.urls.full;
+    img.alt = pic.alt_description;
+    container.appendChild(img);
+    console.log(img);
+  });
+};
+form.onsubmit = async (e) => {
+  container.innerHTML = "";
+  e.preventDefault();
+  if (searchValue.value) {
+    let data = await getData(searchValue.value);
+    createElement(data);
+    showMore.classList.toggle("d-none");
+  }
+};
+showMore.onclick = async () => {
+  page++;
+  let data = await getData(searchValue.value);
+  createElement(data);
+};
